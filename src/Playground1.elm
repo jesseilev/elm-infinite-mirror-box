ecdf2c6764f15bdce76ad340e4bceaca72fa9880
Jesse Levine <jesselevine@Jesses-MBP.cable.rcn.com>
2021-11-30 22:48:11 -0500

    polygon room; detect intersection with any wall

--- src/Playground1.elm
+++ src/Playground1.elm
@@ -3,18 +3,21 @@ module Playground1 exposing (..)
 import Angle
 import Browser
 import Circle2d
+import Element as El exposing (Element)
+import Element.Border as Border
 import Frame2d exposing (Frame2d)
 import Geometry.Svg as Svg
 import Html exposing (Html)
 import LineSegment2d exposing (LineSegment2d)
+import Maybe.Extra as Maybe
+import Pixels exposing (Pixels)
 import Point2d exposing (Point2d)
-import Quantity
+import Polygon2d exposing (Polygon2d)
+import Polyline2d exposing (Polyline2d)
+import Quantity exposing (Quantity)
 import Svg exposing (Svg)
 import Svg.Attributes as Attr
-import Element as El exposing (Element)
-import Element.Border as Border
 import Vector2d exposing (Vector2d)
-import Pixels exposing (Pixels)
 
 main = 
     Browser.sandbox
@@ -28,16 +31,24 @@ main =
 type TopLeftCoords = TopLeftCoords 
 
 type alias Model = 
-    { angle : Float 
+    { roomShape : Polygon2d Pixels TopLeftCoords
     , catFrame : Frame2d Pixels TopLeftCoords {}
+    , targetDistance : Quantity Float Pixels
     }
 
 init = 
-    { angle = 25 
+    { roomShape = 
+        Polygon2d.singleLoop 
+            [ Point2d.pixels 50 50 
+            , Point2d.pixels 350 70
+            , Point2d.pixels 425 300
+            , Point2d.pixels 100 450
+            ]
     , catFrame = 
         Frame2d.atOrigin
-            |> Frame2d.translateBy (Vector2d.pixels 50 50)
-            |> Frame2d.rotateBy (Angle.degrees 65)
+            |> Frame2d.translateBy (Vector2d.pixels 90 150)
+            |> Frame2d.rotateBy (Angle.degrees 315)
+    , targetDistance = Pixels.float 300
     }
 
         
@@ -75,15 +86,31 @@ diagram model =
         catLocation = 
             Frame2d.originPoint model.catFrame
 
-        watchDistance = Pixels.float 350
-
         mirrorLine =
             LineSegment2d.from 
                 (Point2d.pixels 0 100)
                 (Point2d.pixels 300 100)
+
+        mirrorLine2 = 
+            LineSegment2d.from 
+                (Point2d.pixels 0 300)
+                (Point2d.pixels 300 300)
+
         sightLine = 
             LineSegment2d.fromPointAndVector catLocation
-                (Vector2d.withLength watchDistance (Frame2d.xDirection model.catFrame))
+                (Vector2d.withLength model.targetDistance (Frame2d.xDirection model.catFrame))
+
+        bouncePoint = 
+            model.roomShape
+                |> Polygon2d.edges
+                |> List.map (LineSegment2d.intersectionPoint sightLine)
+                |> Maybe.orList
+
+        drawWall = 
+            Svg.lineSegment2d
+                [ Attr.stroke "grey" 
+                , Attr.width "2"
+                ]
     in
     Svg.g 
         []
@@ -92,15 +119,16 @@ diagram model =
             , Attr.width "2"
             ]
             sightLine
-        , Svg.lineSegment2d
-            [ Attr.stroke "black" 
-            , Attr.width "2"
+        , Svg.polygon2d 
+            [ Attr.stroke "orange"
+            , Attr.strokeWidth "4"
+            , Attr.fill "none"
             ]
-            mirrorLine
-        , LineSegment2d.intersectionPoint sightLine mirrorLine
+            model.roomShape
+        , bouncePoint
             |> Maybe.map (\p -> 
-                Svg.circle2d [ Attr.fill "yellow" ]
-                    (Circle2d.atPoint p (Pixels.float 2))
+                Svg.circle2d [ Attr.fill "cyan" ]
+                    (Circle2d.atPoint p (Pixels.float 5))
             )
             |> Maybe.withDefault svgEmtpy
         , Svg.circle2d [ Attr.fill "purple" ]
@@ -108,3 +136,8 @@ diagram model =
         ]
 
 svgEmtpy = Svg.g [] []
+
+
+-- bouncePath : Polygon2d -> Point2d -> Direction2d -> Polyline2d
+-- bouncePath wallShape position gazeDirection = 
+--     Polyline2d.fromVertices []
---
+++ todo.md
@@ -0,0 +1,8 @@
+
+# Todo
+
+- non-pixel units
+- mouse controls eye direction
+- compute sightline bounce
+    - hand-compute intersection point and angle?
+    
